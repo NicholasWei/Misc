87b6b9cbb3fb986526d83fda225fc6960f41098b
Nicholas <nicholas.cwei@gmail.com>
2018-06-24 10:43:15 +0800

    2

---
+++ downloader.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3.6
+
+import urllib.request
+import ssl
+
+prefix = 'https://vod.mudu.tv/watch/2018-06-23/%s'
+
+file = open('todo', 'r')
+ssl._create_default_https_context = ssl._create_unverified_context
+for line in file:
+    todownload = prefix % line
+    print(todownload)
+    print(line)
+    #urllib.request.urlretrieve(todownload, line)
+
+file.close()
+
